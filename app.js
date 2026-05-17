b0e54957db4bc1c2a9a1ab60e1ae8061dd79458c
luiz0ph <luizphsantana@gmail.com>
2026-05-17 14:36:27 -0300

    Configured app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+require("dotenv").config();
+
+const { urlencoded } = require("body-parser");
+const express = require("express");
+const path = require("node:path");
+const cors = require("cors");
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+const app = express();
+
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server running!`);
+    console.log(`http://${HOST}:${PORT}`);
+})
